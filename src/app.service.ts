32e9e48fc5fbd839fe90a3ab311dd4cf59b1695b
kkznch <kkznch710md@gmail.com>
2022-07-16 16:27:35 +0900

    5-2

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+const data = [
+  { name: 'taro', mail: 'taro@yamada', age: 39 },
+  { name: 'jiro', mail: 'jiro@suzuki', age: 29 },
+  { name: 'saburo', mail: 'saburo@sasaki', age: 19 },
+];
+
 @Injectable()
 export class AppService {
-  getHello(): object {
+  getHello(id: number): object {
+    const n = id < 0 ? 0 : id >= data.length ? data.length - 1 : id;
     return {
-      message: 'Hello World!',
-      date: new Date(),
+      id: n,
+      data: data[n],
+      created: new Date(),
     };
   }
 }
