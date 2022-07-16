9aaf587bb58b23b82d8a1c4ba9173fbb83df247e
kkznch <kkznch710md@gmail.com>
2022-07-16 15:43:52 +0900

    5-1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hogehoge')
-  getHello(): string {
+  @Get()
+  getHello(): object {
     return this.appService.getHello();
   }
 }
